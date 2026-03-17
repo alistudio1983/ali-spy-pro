002499c591e2a596d5d2086516d6ca3b3eea349c
alistudio1983 <aboukhbiz7@gmail.com>
2026-03-17 00:58:01 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: 'all',
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
